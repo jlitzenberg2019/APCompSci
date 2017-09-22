dcaf0fffc3d12a59c5da882557cb2679987d3ba8
jlitzenberg2019 <jlitzenberg2019@cchsraider.net>
2017-09-22 10:08:03 -0400

    initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,22 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubs
+		System.out.println("               d");
+		System.out.println("        o     l");
+		System.out.println("       l     r");
+		System.out.println("        l     o");
+		System.out.println("       e     w      ");
+		System.out.println("     h              ");
+		System.out.println(" xxxxxxxxxxxxxxxxxxx");
+		System.out.println(" x              x   x");
+		System.out.println(" x     Java     x   x");
+		System.out.println(" x      is      xxxx ");
+		System.out.println(" x     Spicy     x   ");
+		System.out.println(" x              x");
+		System.out.println(" x              x");
+		System.out.println(" xxxxxxxxxxxxxxxx");
+	}
+
+}
